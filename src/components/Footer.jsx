ad8918a52efb4c4949bad0cd9ddae0dcd980e7a4
Naveen Kumar <nk24528.nk81@gmail.com>
2024-08-03 22:17:13 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,8 @@ import {
   AccordionButton,
   AccordionIcon,
   AccordionPanel,
+  Text,
+  Input,
 } from "@chakra-ui/react";
 import {
   FaFacebook,
@@ -24,6 +26,178 @@ import React from "react";
 function Footer() {
   return (
     <>
+      <Box p={10}>
+        <Heading fontSize={18}>
+          Stay Healthy with Tata 1mg: Your Favourite Online Pharmacy and
+          Healthcare Platform
+        </Heading>
+        <br />
+        <Heading fontSize={16}> We Bring Care to Health.</Heading>
+        <br />
+        Tata 1mg is India's leading digital healthcare platform. From doctor
+        consultations on chat to online pharmacy and lab tests at home: we have
+        it all covered for you. Having delivered over 25 million orders in 1000+
+        cities till date, we are on a mission to bring "care" to "health" to
+        give you a flawless healthcare experience.
+        <br />
+        <Heading fontSize={16}>
+          {" "}
+          <br />
+          <br />
+          Tata 1mg: Your Favourite Online Pharmacy!
+        </Heading>
+        <br />
+       
+        Tata 1mg is India's leading online chemist with over 2 lakh medicines
+        available at the best prices. We are your one-stop destination for other
+        healthcare products as well, such as over the counter pharmaceuticals,
+        healthcare devices and homeopathy and ayurveda medicines.
+        <br />
+        <br />
+        With Tata 1mg, you can buy medicines online and get them delivered at
+        your doorstep anywhere in India! But, is ordering medicines online a
+        difficult process? Not at all! Simply search for the products you want
+        to buy, add to cart and checkout. Now all you need to do is sit back as
+        we get your order delivered to you.
+        <br />
+        <br />
+        In case you need assistance, just give us a call and we will help you
+        complete your order.
+        <br />
+        <br />
+        And there is more! At Tata 1mg, you can buy health products and
+        medicines online at best discounts.
+        <br />
+        <br />
+        Now, isn't that easy? Why go all the way to the medicine store and wait
+        in line, when you have Tata 1mg Pharmacy at your service.
+        <br />
+        <br />
+        <Heading fontSize={16}>The Feathers in Our Cap</Heading>
+        <br />
+        At Tata 1mg, our goal is to make healthcare understandable, accessible
+        and affordable in India. We set out on our journey in 2015, and have
+        come a long way since then. Along the way, we have been conferred with
+        prestigious titles like BML Munjal Award for 'Business Excellence
+        through Learning and Development', Best Online Pharmacy in India Award
+        and Top 50 venture in The Smart CEO-Startup50 India. We have been
+        selected as the only company from across the globe for SD#3 "Health &
+        Well Being for all" by Unreasonable group, US State Department. In 2019
+        alone we received three awards including the BMW Simply Unstoppable
+        Award.
+        <br />
+        <br />
+        <Heading fontSize={16}>The Services We Offer</Heading>
+        <br />
+       
+        Tata 1mg is India's leading digital healthcare platform, where you can
+        buy medicines online with discount. Buy medicine online in Delhi,
+        Mumbai, Bangalore, Hyderabad, Pune, Gurgaon, Noida, Kolkata, Chennai,
+        Ahmedabad, Lucknow and around a 1000 more cities. Besides delivering
+        your online medicine order at your doorstep, we provide accurate,
+        authoritative & trustworthy information on medicines and help people use
+        their medicines effectively and safely.
+        <br />
+        <br />
+        We also facilitate lab tests at home. You can avail over 2,000 tests and
+        get tested by 120+ top and verified labs at the best prices. Need to
+        consult a doctor? On our platform, you can talk to over 20 kinds of
+        specialists in just a few clicks.
+        <br />
+        <br />
+        Customer centricity is the core of our values. Our team of highly
+        trained and experienced doctors, phlebotomists and pharmacists looks
+        into each order to give you a fulfilling experience.
+        <br />
+        <br />
+        We’ve made healthcare accessible to millions by giving them quality care
+        at affordable prices. Customer centricity is the core of our values. Our
+        team of highly trained and experienced doctors, phlebotomists and
+        pharmacists looks into each order to give you a fulfilling experience.
+        <br />
+        <br />
+        Visit our online medical store now, and avail online medicine purchase
+        at a discount. Stay Healthy!
+        <br />
+      </Box>
+      {/* Statistics */}
+      <Box m={"auto"}>
+        <br />
+        <hr style={{ border: "1px solid #d1d1d1" }} />
+        <br />
+
+        <Heading textAlign={"center"} fontWeight={"400"} fontSize={"24px"}>
+          INDIA’S LARGEST HEALTHCARE PLATFORM
+        </Heading>
+        <br />
+
+        <Box
+          w={"90%"}
+          // border={"1px solid black"}
+          display={"grid"}
+          gridTemplateColumns={"repeat(3, 1fr)"}
+          justifyContent={"space-around"}
+          gap={10}
+          alignItems={"center"}
+          m={"auto"}
+        >
+          <Box>
+            <Heading textAlign={"center"}>260m+</Heading>
+            <Text textAlign={"center"}>Visitors</Text>
+          </Box>
+          <Box>
+            <Heading textAlign={"center"}>31m+</Heading>
+            <Text textAlign={"center"}>Orders Delivered</Text>
+          </Box>
+          <Box>
+            <Heading textAlign={"center"}>1800+</Heading>
+            <Text textAlign={"center"}>Cities</Text>
+          </Box>
+        </Box>
+
+        {/* Sign up Form */}
+        <br />
+        <br />
+        <hr style={{ border: "1px solid #d1d1d1" }} />
+        <br />
+        <Box
+          display={"flex"}
+          justifyContent={"space-around"}
+          alignItems={"center"}
+          // gap={20}
+
+          m={"auto"}
+        >
+          <Box>
+            <Heading fontSize={16}>Get the link to download App</Heading>
+          </Box>
+          <Box
+            display={"flex"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+            gap={2}
+          >
+            <Input
+              w={"150%"}
+              p={5}
+              type="text"
+              placeholder="Enter Phone Number"
+            />
+            <Button
+              width={"60%"}
+              _hover={{ background: "tomato" }}
+              bg={"#f5837a"}
+              color={"white"}
+            >
+              Send Link
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+
+      <br />
+      <br />
+      {/* Footer */}
       <Box bg={"#edede8"} p={12}>
         <SimpleGrid
           columns={{
@@ -244,7 +418,7 @@ function Footer() {
         </SimpleGrid>
 
         <br />
-        <hr style={{ border: "1px solid grey" }} />
+        <hr style={{ border: "1px solid #b6b6b8" }} />
         <br />
         <Box
           display={"flex"}
@@ -345,7 +519,13 @@ function Footer() {
             Cosmetics Rules, 1945. We do not process requests for Schedule X and
             habit forming drugs.
           </Box>
-          <Box display={"flex"} w={"20%"} justifyContent={"space-between"} alignItems={"center"} gap={2}>
+          <Box
+            display={"flex"}
+            w={"20%"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            gap={2}
+          >
             <Box>
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/0/04/Visa.svg"
